9bbd0256a70f20139cd04db458ac1004829259f8
Hamzah Contreras <Hamzah_36@outlook.com>
2022-01-11 10:26:38 -0600

    commit tree 795c917526b5c6b3926c439961f46015867d6d39

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const divide = function() {
-    return 2000 / 100;
+    return (2000 / 100)
 }
 
-const square = num1 => num1 * num1; 
+const square = num => num * num
 
-
-const add = (num1, num2) => num1 + num2;
+const add = (num1, num2) => num1 + num2
